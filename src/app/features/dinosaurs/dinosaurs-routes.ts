c4c530173377ad60586780bd898dd5fba2838559
lmagnette <loicmagnette@gmail.com>
2023-09-30 17:56:29 +0200

    #4 add resolver as func

--- src/app/features/dinosaurs/dinosaurs-routes.ts
+++ src/app/features/dinosaurs/dinosaurs-routes.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 import { DinoListComponent } from './components/dino-list/dino-list.component';
 import { DinoDetailsComponent } from './components/dino-details/dino-details.component';
 import { BetterDinoDetailsComponent } from './components/better-dino-details/better-dino-details.component';
 import { abTestingGuard } from './ab-testing.guard';
+import { inject } from '@angular/core';
+import { DinoService } from './services/dino.service';
 
 
 export const DINOSAURS_ROUTES: Routes = [
@@ -13,5 +15,12 @@ export const DINOSAURS_ROUTES: Routes = [
   },
   { path: ':id',
     component: BetterDinoDetailsComponent,
+    resolve:{
+      dino : (route:ActivatedRouteSnapshot) => {
+        const number = Number.parseInt(route.data['id']);
+        return inject(DinoService).get(number);
+      }
+    }
+
   },
 ];
