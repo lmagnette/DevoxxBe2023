39f81470d992fc63a869e042ab6cc01f0afa143d
lmagnette <loicmagnette@gmail.com>
2023-09-30 17:23:09 +0200

    add images, fix data and adapt page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './features/navigation/navbar/navbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
+@NgModule( {
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NavbarComponent
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavbarComponent,
+    NoopAnimationsModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  bootstrap: [ AppComponent ]
+} )
+export class AppModule {
+}
--- src/app/features/dinosaurs/services/dino.service.ts
+++ src/app/features/dinosaurs/services/dino.service.ts
@@ -15,7 +15,7 @@ export class DinoService {
     height: 5,
     period: 'Late Cretaceous',
     diet: 'Carnivore',
-    image: 'https://img.freepik.com/vecteurs-libre/tyrannosaurus-rex-t-rex-style-cartoon_1308-87430.jpg?w=1800&t=st=1695372245~exp=1695372845~hmac=9a4101550d7b9e68b8f17bd579fe081e8854d759149a37f8c51e7da40b664ed8',
+    image: './assets/dino-11.png'
 
   },
     {
@@ -25,7 +25,7 @@ export class DinoService {
       height: 0.5,
       period: 'Late Cretaceous',
       diet: 'Carnivore',
-      image: 'https://res.cloudinary.com/teepublic/image/private/s--_4YqqN4Y--/c_crop,x_10,y_10/c_fit,h_1109/c_crop,g_north_west,h_1260,w_1260,x_-127,y_-76/co_rgb:ffffff,e_colorize,u_Misc:One%20Pixel%20Gray/c_scale,g_north_west,h_1260,w_1260/fl_layer_apply,g_north_west,x_-127,y_-76/bo_157px_solid_white/e_overlay,fl_layer_apply,h_1260,l_Misc:Art%20Print%20Bumpmap,w_1260/e_shadow,x_6,y_6/c_limit,h_1254,w_1254/c_lpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1667491356/production/designs/36231413_0.jpg',
+      image: './assets/dino-12.png',
       description: 'Velociraptor is a genus of small dromaeosaurid dinosaur that lived in Asia during the Late Cretaceous epoch, about 75 million to 71 million years ago. Two species are currently recognized, although others have been assigned in the past.'
     },
     {
@@ -35,7 +35,7 @@ export class DinoService {
       height: 3,
       period: 'Late Cretaceous',
       diet: 'Herbivore',
-      image: 'https://img.freepik.com/vecteurs-libre/dino-triceratops-mignon-illustration-icone-vecteur-dessin-anime-camera-technologie-animale-isolee-plat_138676-6754.jpg?w=2000&t=st=1695372283~exp=1695372883~hmac=88e4e798f353e860d36549ae0df0ab76f2db4b7c28a7a00dccc5dc404f6d21a4',
+      image: './assets/dino-2.png',
       description: 'Triceratops is a genus of chasmosaurine ceratopsian dinosaur that lived during the late Maastrichtian age of the Late Cretaceous period, about 68 to 66 million years ago in what is now western North America. It was one of the last-known non-avian dinosaurs and lived until the Cretaceous–Paleogene extinction event 66 million years ago'
     },
     {
@@ -45,7 +45,7 @@ export class DinoService {
       height: 15,
       period: 'Late Jurassic',
       diet: 'Herbivore',
-      image: 'https://img.freepik.com/vecteurs-libre/personnage-dessin-anime-brachiosaurus-isole_1308-133696.jpg?w=2000&t=st=1695372604~exp=1695373204~hmac=8fb4211a6023baba94ded6d5d2e050a84f992f15646a99a6680350bef40598b8',
+      image: './assets/dino-7.png',
       description: 'Brachiosaurus is a genus of sauropod dinosaur that lived in North America during the Late Jurassic, about 154 to 150 million years ago. It was first described by American paleontologist Elmer S. Riggs in 1903 from fossils found in the Colorado River valley in western Colorado, United States. '
     },
     {
@@ -55,7 +55,7 @@ export class DinoService {
       height: 4,
       period: 'Late Jurassic',
       diet: 'Herbivore',
-      image: 'https://img.freepik.com/vecteurs-libre/illustration-stegosaure-dessin-anime-dessine-main_23-2150497689.jpg?w=2000&t=st=1695372642~exp=1695373242~hmac=a6edf25fefb67756741310a753e17d8fe2acaa0f5e5ccaee1505543a9b9d5c84',
+      image: './assets/dino-3.png',
       description: 'Stegosaurus is a genus of herbivorous, four-legged, armored dinosaur from the Late Jurassic, characterized by the distinctive kite-shaped upright plates along their backs and spikes on their tails. Fossils of the genus have been found in the western United States and in Portugal'
     },
     {
@@ -65,38 +65,38 @@ export class DinoService {
       height: 4,
       period: 'Late Jurassic',
       diet: 'Herbivore',
-      image: 'https://img.freepik.com/vecteurs-libre/illustration-stegosaure-dessin-anime-dessine-main_52683-118924.jpg?w=2000&t=st=1695372351~exp=1695372951~hmac=6b9a41422218ecce564d3cd7768a3c69958567ff4bc7f249057ed056595d7fe0',
+      image: './assets/dino-9.png',
       description: 'Diplodocus was a genus of diplodocid sauropod dinosaurs, whose fossils were first discovered in 1877 by S. W. Williston. The generic name, coined by Othniel Charles Marsh in 1878, is a Neo-Latin term derived from Greek διπλός (diplos) "double" and δοκός (dokos) "beam", in reference to the double-beamed chevron bones located in the underside of the tail, which were then considered unique.'
     },
     {
       id: 7,
-      name: 'Allosaurus',
+      name: 'Parasaurolophus',
       weight: 2000,
       height: 3,
       period: 'Late Jurassic',
-      diet: 'Carnivore',
-      image: 'https://pbs.twimg.com/media/EFJ00Z9XYAEqrrX?format=jpg&name=large',
-      description: 'Allosaurus is an extinct genus of large carnosaurian theropod dinosaur that lived 155 to 145 million years ago during the Late Jurassic period (Kimmeridgian to late Tithonian ages). The name "Allosaurus" means "different lizard", alluding to its unique (at the time of its discovery) concave vertebrae. It is derived from the Greek words ἄλλος (allos) ("different", "strange", or "other") and σαῦρος (sauros) ("lizard" or "reptile"). '
+      diet: 'Herbivore',
+      image: './assets/dino-5.png',
+      description: 'Parasaurolophus (/ˌpærəsɔːˈrɒləfəs, -ˌsɔːrəˈloʊfəs/; meaning "near crested lizard" in reference to Saurolophus) is a genus of hadrosaurid "duck-billed" dinosaur that lived in what is now western North America and possibly Asia during the Late Cretaceous period, about 76.5–73 million years ago.'
     },
     {
       id: 8,
-      name: 'Pteranodon',
+      name: 'Pterodactyl',
       weight: 20,
       height: 1,
       period: 'Late Cretaceous',
-      diet: 'Carnivore',
-      image: 'https://img.freepik.com/vecteurs-libre/illustration-pterodactyle-dessin-anime-dessine-main_23-2150497877.jpg?w=2000&t=st=1695372438~exp=1695373038~hmac=1db6b27c19c3ab61ea4c1deaf511530b3571a3b2289df203b3a25d108f9ac20c',
-      description: 'Pteranodon from Ancient Greek πτερόν (pteron \'wing\') and ἀνόδων (anodon \'toothless\') is a genus of pterosaur that included some of the largest known flying reptiles, with P. longiceps having a wingspan of over 6 m (20 ft). They lived during the late Cretaceous geological period of North America in present-day Kansas, Nebraska, Wyoming, South Dakota and Alabama.'
+      diet: 'Herbivore',
+      image: './assets/dino-1.png',
+      description: 'Pterosaurs (/ˈtɛrəsɔːr, ˈtɛroʊ-/ from Greek pteron and sauros, meaning "wing lizard") are an extinct clade of flying reptiles in the order Pterosauria. They existed during most of the Mesozoic: from the Late Triassic to the end of the Cretaceous (228 to 66 million years ago).[7] Pterosaurs are the earliest vertebrates known to have evolved powered flight.'
     },
     {
       id: 9,
-      name: 'Archaeopteryx',
+      name: 'Apatosaurus',
       weight: 1,
       height: 0.5,
       period: 'Late Jurassic',
-      diet: 'Carnivore',
-      image: 'https://img.freepik.com/vecteurs-libre/dinosaure-archaeopteryx-fond-blanc_1308-89766.jpg?w=2000&t=st=1695372486~exp=1695373086~hmac=39f29e97279504a4d834c567309392977a87f739538e17711af8d0ac3173bdc4',
-      description: 'Archaeopteryx, meaning "old wing" (sometimes referred to by its German name Urvogel ("original bird" or "first bird")), is a genus of bird-like dinosaurs that is transitional between non-avian feathered dinosaurs and modern birds. The name derives from the ancient Greek ἀρχαῖος (archaīos) meaning "ancient", and πτέρυξ (ptéryx), meaning "feather" or "wing". Between the late nineteenth century and the early twenty-first century, Archaeopteryx had been generally accepted by palaeontologists and popular reference books as the oldest known bird (member of the group Avialae).'
+      diet: 'Herbivore',
+      image: './assets/dino-10.png',
+      description: 'Apatosaurus (/əˌpætəˈsɔːrəs/ meaning "deceptive lizard") is a genus of herbivorous sauropod dinosaur that lived in North America during the Late Jurassic period. Othniel Charles Marsh described and named the first-known species, A. ajax, in 1877, and a second species, A. louisae, was discovered and named by William H. Holland in 1916. Apatosaurus lived about 152 to 151 million years ago (mya), during the late Kimmeridgian to early Tithonian age, and are now known from fossils in the Morrison Formation of modern-day Colorado, Oklahoma, New Mexico, Wyoming, and Utah in the United States. '
     },
     {
       id: 10,
@@ -105,7 +105,7 @@ export class DinoService {
       height: 3,
       period: 'Late Cretaceous',
       diet: 'Herbivore',
-      image: 'https://img.freepik.com/vecteurs-libre/dessin-anime-mignon-dinosaure-ankylosaurus_1308-110688.jpg?w=2000&t=st=1695372531~exp=1695373131~hmac=62c50815feb80e0e89ea8f3d71c96267e943a52eb4d24e494dd6f2633a67976f',
+      image: './assets/dino-8.png',
       description: 'Ankylosaurus is a genus of armored dinosaur. Its fossils have been found in geological formations dating to the very end of the Cretaceous Period, about 68–66 million years ago, in western North America, making it among the last of the non-avian dinosaurs. It was named by Barnum Brown in 1908; the only species in the genus is A. magniventris.'
     },
     {
@@ -115,7 +115,7 @@ export class DinoService {
       height: 3,
       period: 'Late Cretaceous',
       diet: 'Carnivore',
-      image: 'https://img.freepik.com/vecteurs-libre/petit-personnage-dessin-anime-dinosaure-spinosaurus-mignon_1308-112984.jpg?w=2000&t=st=1695372567~exp=1695373167~hmac=61dff79e18d24fea0730c7c57b3c20dfa34f5f3eff98b9f994f8f7b53ea388f9',
+      image: './assets/dino-6.png',
       description: 'Spinosaurus is a genus of spinosaurid dinosaur that lived in what now is North Africa during the upper Albian to upper Turonian stages of the Cretaceous period, about 112 to 93.5 million years ago. This genus was known first from Egyptian remains discovered in 1912 and described by German paleontologist Ernst Stromer in 1915. The original remains were destroyed in World War II, but additional material has come to light in recent years. It is unclear whether one or two species are represented in the fossils reported in the scientific literature. The best known species is S. aegyptiacus from Egypt, although a potential second species, S. maroccanus, has been recovered from Morocco.'
     }
   ];
